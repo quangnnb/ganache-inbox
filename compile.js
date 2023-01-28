f7b2c4b2470e947a379ae8dd14a2b22b43a05542
Quang <bnnquang@gmail.com>
2023-01-28 23:01:58 +0700

    update test case

--- compile.js
+++ compile.js
@@ -5,9 +5,9 @@ const fs = require("fs");
 // declare solidity compiler module
 const solc = require("solc");
 
+// config source file 
 const inboxPath = path.resolve(__dirname, "contracts", "Inbox.sol");
-
 const source = fs.readFileSync(inboxPath, "utf8");
 
 // compile the source file
-console.log(solc.compile(source, 1));
+module.exports(solc.compile(source, 1));
--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,15 +1,17 @@
+//declaration
 const assert = require("assert");
 const ganache = require("ganache-cli");
 const Web3 = require("web3");
 const web3 = new Web3(ganache.provider());
 
 beforeEach(() => {
-  // Get a list of all accounts
-  accounts = web3.eth.getAccounts();
+  // Get a list of all accounts pre-set in ganache network automatically
+  web3.eth.getAccounts().then( fetchedAccounts => {
+    console.log(fetchedAccounts);
+  });
+  
 });
 
 describe("Inbox", () => {
-  it("deploys a contract", () => {
-    console.log(accounts);
-  });
+  it("deploys a contract", () => {});
 });
